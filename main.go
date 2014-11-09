4622ab7aee1653d88e412d7fa1045b9eb68a94f6
Yuriy Nasretdinov <y.nasretdinov@corp.badoo.com>
2014-11-09 10:47:59 +0300

    Changes in part 1

--- main.go
+++ main.go
@@ -5,12 +5,14 @@ import (
 	"crypto/md5"
 	"crypto/sha1"
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -19,24 +21,87 @@ const (
 	EVENT_USER_CONNECTED = iota
 	EVENT_USER_DISCONNECTED
 	EVENT_ONLINE_USERS_LIST
+	EVENT_USER_REPLY
+
+	REQUEST_GET_MESSAGES = iota
+
+	REPLY_ERROR = iota
+	REPLY_MESSAGES_LIST
+
+	MAX_MESSAGES_LIMIT = 100
 )
 
 type (
-	Event struct {
-		evType uint8
-		evData string
+	BaseEvent struct {
+		Type string
+	}
+
+	UserInfo struct {
+		Name string
+		Id   string
+	}
+
+	BaseRequest struct {
+		SeqId   int
+		Type    string
+		ReqData string
+	}
+
+	RequestGetMessages struct {
+		UserTo  uint64
+		DateEnd uint64
+		Limit   uint64
+	}
+
+	BaseReply struct {
+		SeqId int
+		Type  string
+	}
+
+	Message struct {
+		Id   uint64
+		Text string
+		Ts   uint64
+	}
+
+	ReplyGetMessages struct {
+		BaseReply
+		Messages []Message
+	}
+
+	ReplyError struct {
+		BaseReply
+		Message string
+	}
+
+	EventUserConnected struct {
+		BaseEvent
+		UserInfo
+	}
+
+	EventUserDisconnected struct {
+		BaseEvent
+		UserInfo
+	}
+
+	EventOnlineUsersList struct {
+		BaseEvent
+		Users []UserInfo
 	}
 
 	ControlEvent struct {
 		evType   uint8
 		info     map[string]string
-		listener chan *Event
+		reply    interface{}
+		listener chan interface{}
 	}
 )
 
 var (
-	db        *sql.DB
-	loginStmt *sql.Stmt
+	db              *sql.DB
+	loginStmt       *sql.Stmt
+	getMessagesStmt *sql.Stmt
+	sendMessageStmt *sql.Stmt
 
 	eventsFlow = make(chan *ControlEvent, 200)
 )
@@ -153,31 +218,114 @@ func IndexHandler(w http.ResponseWriter, req *http.Request) {
 }
 
 func EventsDispatcher() {
-	listenerMap := make(map[chan *Event]map[string]string)
+	listenerMap := make(map[chan interface{}]map[string]string)
 
 	for ev := range eventsFlow {
 		if ev.evType == EVENT_USER_CONNECTED {
-			currentUsers := make([]string, 0, len(listenerMap))
+			currentUsers := make([]UserInfo, 0, len(listenerMap))
 			for _, info := range listenerMap {
-				currentUsers = append(currentUsers, info["name"]+" #"+info["id"])
+				currentUsers = append(currentUsers, UserInfo{Name: info["name"], Id: info["id"]})
 			}
 
-			ev.listener <- &Event{evType: EVENT_ONLINE_USERS_LIST, evData: strings.Join(currentUsers, "|")}
+			ouEvent := new(EventOnlineUsersList)
+			ouEvent.Type = "EVENT_ONLINE_USERS_LIST"
+			ouEvent.Users = currentUsers
+			ev.listener <- ouEvent
 
 			listenerMap[ev.listener] = ev.info
 			for listener := range listenerMap {
-				if len(listener) < cap(listener) {
-					listener <- &Event{evType: EVENT_USER_CONNECTED, evData: ev.info["name"] + " #" + ev.info["id"]}
+				if len(listener) >= cap(listener) {
+					continue
 				}
+
+				event := new(EventUserConnected)
+				event.Type = "EVENT_USER_CONNECTED"
+				event.Name = ev.info["name"]
+				event.Id = ev.info["id"]
+				listener <- event
 			}
 		} else if ev.evType == EVENT_USER_DISCONNECTED {
 			delete(listenerMap, ev.listener)
 			for listener := range listenerMap {
-				if len(listener) < cap(listener) {
-					listener <- &Event{evType: EVENT_USER_DISCONNECTED, evData: ev.info["name"] + " #" + ev.info["id"]}
+				if len(listener) >= cap(listener) {
+					continue
 				}
+
+				event := new(EventUserDisconnected)
+				event.Type = "EVENT_USER_DISCONNECTED"
+				event.Name = ev.info["name"]
+				event.Id = ev.info["id"]
+				listener <- event
 			}
+		} else if ev.evType == EVENT_USER_REPLY {
+			if _, ok := listenerMap[ev.listener]; !ok {
+				continue
+			}
+
+			if len(ev.listener) >= cap(ev.listener) {
+				continue
+			}
+
+			ev.listener <- ev.reply
 		}
+	}
+}
+
+func sendError(seqId int, recvChan chan interface{}, message string) {
+	reply := new(ReplyError)
+	reply.SeqId = seqId
+	reply.Type = "REPLY_ERROR"
+	reply.Message = message
+
+	eventsFlow <- &ControlEvent{
+		evType:   EVENT_USER_REPLY,
+		listener: recvChan,
+		reply:    reply,
+	}
+}
+
+func processGetMessages(getMsgReq *RequestGetMessages, seqId int, recvChan chan interface{}, userId uint64) {
+	dateEnd := getMsgReq.DateEnd
+	if dateEnd == 0 {
+		dateEnd = uint64(time.Now().Unix())
+	}
+
+	limit := getMsgReq.Limit
+	if limit > MAX_MESSAGES_LIMIT {
+		limit = MAX_MESSAGES_LIMIT
+	}
+
+	if limit <= 0 {
+		sendError(seqId, recvChan, "Limit must be greater than 0")
+		return
+	}
+
+	rows, err := getMessagesStmt.Query(userId, getMsgReq.UserTo, dateEnd, limit)
+	if err != nil {
+		sendError(seqId, recvChan, "Cannot select messages")
+		log.Println(err.Error())
+		return
+	}
+
+	reply := new(ReplyGetMessages)
+	reply.SeqId = seqId
+	reply.Type = "REPLY_MESSAGES_LIST"
+	reply.Messages = make([]Message, 0)
+
+	for rows.Next() {
+		var msg Message
+		if err = rows.Scan(&msg.Id, &msg.Text, &msg.Ts); err != nil {
+			log.Println(err.Error())
+			return
+		}
+
+		reply.Messages = append(reply.Messages, msg)
+	}
+
+	eventsFlow <- &ControlEvent{
+		evType:   EVENT_USER_REPLY,
+		listener: recvChan,
+		reply:    reply,
 	}
 }
 
@@ -189,23 +337,40 @@ func EventsHandler(ws *websocket.Conn) {
 		return
 	}
 
-	recvChan := make(chan *Event, 100)
+	userId, err := strconv.ParseUint(info["id"], 10, 64)
+	if err != nil {
+		ws.Write([]byte("Cannot parse user id"))
+		log.Println("Corrupt memcache user info: ", err.Error())
+		return
+	}
+
+	recvChan := make(chan interface{}, 100)
 	eventsFlow <- &ControlEvent{evType: EVENT_USER_CONNECTED, info: info, listener: recvChan}
 	defer func() { eventsFlow <- &ControlEvent{evType: EVENT_USER_DISCONNECTED, info: info, listener: recvChan} }()
 
 	go func() {
-		var msg [100]byte
 		for {
-			n, err := ws.Read(msg[:])
-			if err == nil {
-				fmt.Println("Read from " + info["name"] + ": " + string(msg[:n]))
-			} else {
+			req := new(BaseRequest)
+
+			if err := websocket.JSON.Receive(ws, req); err != nil {
 				ws.Close()
 				recvChan <- nil
 				return
 			}
-		}
 
+			if req.Type == "REQUEST_GET_MESSAGES" {
+				getMsgReq := new(RequestGetMessages)
+				if err := json.Unmarshal([]byte(req.ReqData), getMsgReq); err != nil {
+					sendError(req.SeqId, recvChan, "Cannot unmarshal: "+err.Error())
+					continue
+				}
+
+				go processGetMessages(getMsgReq, req.SeqId, recvChan, userId)
+			} else {
+				sendError(req.SeqId, recvChan, "Invalid request type: "+req.Type)
+				continue
+			}
+		}
 	}()
 
 	for ev := range recvChan {
@@ -213,18 +378,8 @@ func EventsHandler(ws *websocket.Conn) {
 			return
 		}
 
-		var evTypeStr string
-		if ev.evType == EVENT_USER_CONNECTED {
-			evTypeStr = "EVENT_USER_CONNECTED"
-		} else if ev.evType == EVENT_USER_DISCONNECTED {
-			evTypeStr = "EVENT_USER_DISCONNECTED"
-		} else if ev.evType == EVENT_ONLINE_USERS_LIST {
-			evTypeStr = "EVENT_ONLINE_USERS_LIST"
-		}
-
-		_, err := ws.Write([]byte(fmt.Sprintf("%s:%s", evTypeStr, ev.evData)))
-		if err != nil {
-			fmt.Println("Write error: " + err.Error())
+		if err := websocket.JSON.Send(ws, ev); err != nil {
+			fmt.Println("Could not send JSON: " + err.Error())
 			return
 		}
 	}
@@ -232,6 +387,31 @@ func EventsHandler(ws *websocket.Conn) {
 
 func init() {
 	initSession()
+}
+
+func initStmts(db *sql.DB) {
+	var err error
+
+	loginStmt, err = db.Prepare("SELECT id, password, name FROM User WHERE email = ?")
+	if err != nil {
+		log.Fatal("Could not prepare email select: " + err.Error())
+	}
+
+	getMessagesStmt, err = db.Prepare(`SELECT id, message, UNIX_TIMESTAMP(ts)
+		FROM Messages
+		WHERE user_id = ? AND user_id_to = ? AND ts <= FROM_UNIXTIME(?)
+		ORDER BY ts DESC
+		LIMIT ?`)
+	if err != nil {
+		log.Fatal("Could not prepare messages select: " + err.Error())
+	}
+
+	sendMessageStmt, err = db.Prepare(`INSERT INTO Messages
+		(user_id, user_id_to, msg_type, message, ts)
+		VALUES(?, ?, ?, ?, NOW())`)
+	if err != nil {
+		log.Fatal("Could not prepare messages select: " + err.Error())
+	}
 }
 
 func main() {
@@ -242,10 +422,7 @@ func main() {
 		log.Fatal("Could not connect to db: " + err.Error())
 	}
 
-	loginStmt, err = db.Prepare("SELECT id, password, name FROM User WHERE email = ?")
-	if err != nil {
-		log.Fatal("Could not prepare email select: " + err.Error())
-	}
+	initStmts(db)
 
 	http.Handle("/events", websocket.Handler(EventsHandler))
 	go EventsDispatcher()
@@ -254,7 +431,6 @@ func main() {
 	http.HandleFunc("/login", LoginHandler)
 	http.HandleFunc("/logout", LogoutHandler)
 	http.HandleFunc("/", IndexHandler)
-	fmt.Println("Hello world!")
 	err = http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
--- static/main.js
+++ static/main.js
@@ -1,36 +1,136 @@
-var allUsers = {};
+var allUsers = {}
+var seqId = 0
+var rcvCallbacks = {}
+var websocket
+
+function onUserConnect(userInfo) {
+	var userId = userInfo.Id
+	if (allUsers[userId]) {
+		allUsers[userId].cnt++
+	} else {
+		allUsers[userId] = userInfo
+		allUsers[userId].cnt = 1
+	}
+}
+
+function onUserDisconnect(userInfo) {
+	var userId = userInfo.Id
+	if (allUsers[userId]) {
+		allUsers[userId].cnt--
+		if (allUsers[userId].cnt <= 0) {
+			delete(allUsers[userId])
+		}
+	}
+}
 
 function onMessage(evt) {
-	console.log(evt.data);
-	var parts = evt.data.split(/:/)
-	if (parts[0] == 'EVENT_ONLINE_USERS_LIST') {
-		if (parts[1].length > 0) {
-			var pparts = parts[1].split(/\|/g)
-			for (var i = 0; i < pparts.length; i++) {
-				allUsers[pparts[i]] = true;
-			}
+	var reply = JSON.parse(evt.data)
+	if (reply.Type == 'EVENT_ONLINE_USERS_LIST') {
+		for (var i = 0; i < reply.Users.length; i++) {
+			onUserConnect(reply.Users[i])
 		}
-	} else if (parts[0] == 'EVENT_USER_CONNECTED') {
-		allUsers[parts[1]] = true;
-	} else if (parts[0] == 'EVENT_USER_DISCONNECTED') {
-		delete(allUsers[parts[1]]);
+	} else if (reply.Type == 'EVENT_USER_CONNECTED') {
+		onUserConnect(reply)
+	} else if (reply.Type == 'EVENT_USER_DISCONNECTED') {
+		onUserDisconnect(reply)
+	} else {
+		if (!rcvCallbacks[reply.SeqId]) {
+			console.log("Received response for missing seqid")
+			console.log(reply)
+		} else {
+			rcvCallbacks[reply.SeqId](reply)
+			delete(rcvCallbacks[reply.SeqId])
+		}
 	}
 
-	redrawUsers();
+	redrawUsers()
+}
+
+function sendReq(req, onrcv) {
+	var ourSeqId = seqId
+	seqId++
+	req.SeqId = ourSeqId
+	websocket.send(JSON.stringify(req))
+	rcvCallbacks[ourSeqId] = onrcv
+}
+
+function showMessagesResponse(reply) {
+	var messages = []
+	var len = reply.Messages.length
+	var haveMore = false
+	if (len > 5) {
+		len = 5
+		messages.push("...")
+	}
+	for (var i = len - 1; i >= 0; i--) {
+		var msg = reply.Messages[i]
+		messages.push('<div class="message">' + msg.Text + '</div>')
+	}
+
+	document.getElementById("messages_texts").innerHTML = messages.join('')
+}
+
+function showMessages(id) {
+	sendReq(
+		{Type: "REQUEST_GET_MESSAGES", ReqData: JSON.stringify({
+			UserTo: +id,
+			Limit: 6,
+		})},
+		showMessagesResponse
+	)
 }
 
 function redrawUsers() {
-	var str = 'online users';
-	for (var userName in allUsers) {
-		str += '<br/>' + userName;
+	var str = '<b>online users</b>'
+	var msgUsers = []
+
+	for (var userId in allUsers) {
+		var userInfo = allUsers[userId]
+		str += '<br/>' + userInfo.Name
+		msgUsers.push('<div class="user" id="messages' + userInfo.Id + '">' + userInfo.Name + "</div>")
 	}
-	document.getElementById("online_users").innerHTML = str;
+
+	document.getElementById("online_users").innerHTML = str
+	document.getElementById("users").innerHTML = msgUsers.join(" ")
+	var els = document.getElementsByClassName("user")
+	for (var i = 0; i < els.length; i++) {
+		addEv(els[i].id, 'click', function(ev) {
+			showMessages(ev.target.id.replace('messages', ''))
+		})
+	}
 }
 
 function setWebsocketConnection() {
-	var websocket = new WebSocket("ws://" + window.location.host + "/events");
-	websocket.onopen = function(evt) { console.log("open"); };
-	websocket.onclose = function(evt) { console.log("close"); setTimeout(setWebsocketConnection, 1000); };
-	websocket.onmessage = onMessage;
-	websocket.onerror = function(evt) { console.log("Error: " + evt) };
+	rcvCallbacks = {}
+	websocket = new WebSocket("ws://" + window.location.host + "/events")
+	websocket.onopen = function(evt) { console.log("open") }
+	websocket.onclose = function(evt) { console.log("close"); setTimeout(setWebsocketConnection, 1000) }
+	websocket.onmessage = onMessage
+	websocket.onerror = function(evt) { console.log("Error: " + evt) }
+}
+
+function addEv(id, evName, func) {
+	var el = document.getElementById(id)
+	if (!el) {
+		console.log("el not found: ", id)
+		return false
+	}
+	el.addEventListener(
+		evName,
+		function (ev) {
+			if (func(ev) === false) {
+				ev.preventDefault()
+				ev.stopPropagation()
+			}
+		},
+		true
+	)
+	return true
+}
+
+function setUpPage() {
+	addEv("messages_link", "click", function(ev) {
+		document.getElementsByClassName("messages")[0].style.display = ''
+		return false
+	})
 }
