15c72da0517870047b91681c719b58e5718c63e1
Yuriy Nasretdinov <y.nasretdinov@corp.badoo.com>
2014-11-09 11:24:50 +0300

    Added structure.sql changes (still for part1)

--- structure.sql
+++ structure.sql
@@ -1,6 +1,19 @@
 CREATE DATABASE social;
 USE social;
 
+-- Create syntax for TABLE 'Messages'
+CREATE TABLE `Messages` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) unsigned NOT NULL,
+  `user_id_to` int(10) unsigned NOT NULL,
+  `msg_type` enum('In','Out') NOT NULL DEFAULT 'In',
+  `message` longtext NOT NULL,
+  `ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`,`user_id_to`,`ts`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+
+-- Create syntax for TABLE 'User'
 CREATE TABLE `User` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `email` varchar(255) NOT NULL DEFAULT '',
@@ -8,4 +21,4 @@ CREATE TABLE `User` (
   `name` varchar(255) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`),
   KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
